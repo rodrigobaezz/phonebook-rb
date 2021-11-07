d64c268de4c74cd5dcb45628bb8f63e5b01ac2cc
Rodrigo Baez <rodrigobaez@gm2dev.com>
2021-11-07 12:47:07 -0300

    initial commit

---
+++ src/Phonebook.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Phonebook.css';
+
+class Phonebook extends Component {
+
+  constructor() {
+    super();
+
+  }
+
+  render() {
+
+
+    return (
+
+      
+    );
+  }
+}
+
+export default Phonebook;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Phonebook from './Phonebook';
+
+ReactDOM.render(<Phonebook />, document.getElementById('root'));
